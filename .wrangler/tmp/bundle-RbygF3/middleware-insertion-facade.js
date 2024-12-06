067ddc29d9aaf6048b5b4b50369607d58bed6070
kou <107801494+nomanoma121@users.noreply.github.com>
2024-12-06 11:18:27 +0900

    wranglre.tomlをignoreするのをやめた

---
+++ .wrangler/tmp/bundle-RbygF3/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/home/kouta/dev/todo-app/app/workers";
+				import * as __MIDDLEWARE_0__ from "/home/kouta/.nvm/versions/node/v23.3.0/lib/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/home/kouta/.nvm/versions/node/v23.3.0/lib/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/home/kouta/dev/todo-app/app/workers";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
